80f169fcf6669c2eff728d38102a66e1dff3c675
David Michael Innocent <ldsholder3@gmail.com>
2024-04-26 23:24:40 +0100

    updated

---
+++ scripts/links.js
@@ -0,0 +1,47 @@
+const baseURL = "https://davidmichaelinnocent.github.io/wdd230/";
+
+const linksURL = "https://davidmichaelinnocent.github.io/wdd230/json/links.json";
+
+
+async function getLinks() {
+    try {
+      const response = await fetch(linksURL);
+      const data = await response.json();
+      displayLinks(data.weeks);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getLinks();
+
+
+ 
+  function displayLinks(weeks) {
+    const activitiesContainer = document.querySelector('#activities-container');
+
+    weeks.forEach((week, index) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${week.week}: `;
+      
+      week.links.forEach((link, linkIndex) => {
+        const linkAnchor = document.createElement('a');
+        linkAnchor.href = baseURL + link.url;
+        linkAnchor.textContent = link.title;
+        listItem.appendChild(linkAnchor);
+
+        if (linkIndex < week.links.length - 1) {
+          const separator = document.createTextNode(' | ');
+          listItem.appendChild(separator);
+        }
+      });
+
+      activitiesContainer.appendChild(listItem);
+
+      if (index < weeks.length - 1) {
+        const lineBreak = document.createElement('br');
+        activitiesContainer.appendChild(lineBreak);
+      }
+    });
+  }
+  
