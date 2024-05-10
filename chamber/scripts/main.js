f411b866eb69dda87e16b69cc57a61ce69541e42
David Michael Innocent <ldsholder3@gmail.com>
2024-05-10 23:08:49 +0100

    updated

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -18,7 +18,7 @@ const lastVisit = localStorage.getItem('lastVisit');
     const timeDiff = Math.floor((currentDate - lastVisitDate) / (1000 * 60 * 60 * 24));
 
     if (timeDiff === 0) {
-      sidebarContent.textContent = "Back so soon! Awesome!";
+      sidebarContent.textContent = "We're glad you're here again!";
     } else {
       const timeMessage = `You last visited ${timeDiff} ${timeDiff === 1 ? 'day' : 'days'} ago.`;
       sidebarContent.textContent = timeMessage;
